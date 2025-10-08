e2b07bf6de3dafb9112bfe93d4848480df00d811
rithesh-n <ritheshan18@gmail.com>
2025-10-08 15:18:51 +0530

    fix: cookie for production

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -73,10 +73,11 @@ const login = async (req, res) => {
         const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
             expiresIn: "7d",
         });
+        const isProduction = process.env.NODE_ENV === "production";
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,         // ✅ true in production (HTTPS)
-            sameSite: "Lax",       // Or "None" with HTTPS
+            secure: isProduction,         // ✅ true in production (HTTPS)
+            sameSite: isProduction ? "none" : "lax",       // Or "None" with HTTPS
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
           });
       
