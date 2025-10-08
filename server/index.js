2c701a5fcd2e0e73558a5ea468798f41cf30bc38
rithesh-n <ritheshan18@gmail.com>
2025-10-08 11:56:47 +0530

    fix: update cors

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ const app = express();
 // const router = express.Router();
 
 app.use(cors({
-  origin: "http://localhost:5173", // ✅ Your frontend origin
+  origin: ["http://localhost:5173", "https://vox-biz.vercel.app"], // ✅ Your frontend origin
   credentials: true                // ✅ Required to send/receive cookies
 }));
 app.use(express.json()); // Parse JSON bodies
