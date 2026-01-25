af4824f901b0301f5342715ee1f6f06dffde8c52
rithesh-n <ritheshan18@gmail.com>
2026-01-26 01:22:21 +0530

    fix: update the file path firebase credentials

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -3,6 +3,8 @@ import fs from "fs";
 
 
 let serviceAccount;
+console.log("Secrets directory:", fs.existsSync("/etc/secrets"));
+console.log("Secret files:", fs.existsSync("/etc/secrets/firebase-service-account-credentials.json"));
 if (process.env.NODE_ENV === "production") {
   // ✅ Render Secret File location
   serviceAccount = JSON.parse(
