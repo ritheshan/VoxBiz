dbf0c8d4be87757a2d2c07445196728a616e4320
rithesh-n <ritheshan18@gmail.com>
2026-01-26 01:06:26 +0530

    fix: update the file path firebase credentials

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,16 +1,25 @@
 import admin from "firebase-admin";
 import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-const serviceAccountPath = path.join(__dirname, "../etc/secrets/firebase-service-account-credentials.json");
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf8")
-);
+let serviceAccount;
+if (process.env.NODE_ENV === "production") {
+  // ✅ Render Secret File location
+  serviceAccount = JSON.parse(
+    fs.readFileSync(
+      "/etc/secrets/firebase-service-account-credentials.json",
+      "utf8"
+    )
+  );
+} else {
+  // ✅ Local development file (gitignored)
+  serviceAccount = JSON.parse(
+    fs.readFileSync(
+      new URL("../etc/secrets/firebase-service-account-credentials.json", import.meta.url),
+      "utf8"
+    )
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
